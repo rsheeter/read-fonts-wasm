3fbccda39561c599a50f5a6725952dddae6b07e2
Rod S <rsheeter@google.com>
2023-03-22 09:27:26 -0700

    Read as a font

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use js_sys::ArrayBuffer;
+use js_sys::{ArrayBuffer, Uint8Array};
+use read_fonts::FontRef;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
 }
 
@@ -12,6 +13,19 @@ pub fn greet(name: &str) {
 }
 
 #[wasm_bindgen]
-pub fn read_glyph(font: &ArrayBuffer) -> String {
-    return format!("A font with {} bytes!", font.byte_length());
+pub fn read_glyph(buf: &ArrayBuffer) -> String {
+    let u8_buf = Uint8Array::new(&buf);
+    let mut rust_buf: Vec<u8> = vec![0; u8_buf.byte_length() as usize];
+    u8_buf.copy_to(rust_buf.as_mut_slice());
+
+    let font = match FontRef::new(&rust_buf) {
+        Ok(font) => font,
+        Err(e) => return format!("FontRef::new failed: {e}"),
+    };
+    font.table_directory
+        .table_records()
+        .iter()
+        .map(|tr| tr.tag().to_string())
+        .collect::<Vec<_>>()
+        .join(",")
 }
